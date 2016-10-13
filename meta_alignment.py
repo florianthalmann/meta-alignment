a4f174fe4be6ebbc1ea41957cdcfe3bf02d3c362
Florian Thalmann <florianthalmann@bluewin.ch>
2016-10-13 19:06:30 +0100

    added new slower algorithm that looks at all matches in range and chooses best one, can now plot matrix with confidence values for a pair of recordings

--- meta_alignment.py
+++ meta_alignment.py
@@ -4,10 +4,11 @@ import numpy as np
 from scipy import stats
 from matplotlib import pyplot as plt
 import matplotlib.patches as patches
+import seaborn as sns
 
 
 matchfeature = 'vamp:match-vamp-plugin:match:a_b'
-audiodir = 'audio-light/'
+audiodir = 'audio/'
 featuresdir = 'features/'
 resultsdir = 'results/'
 
@@ -70,15 +71,12 @@ def get_alignment_points(file, reffile):
     rsquared = r_value**2
     #print str(slope)+'x + '+str(intercept), 'r^2: '+str(r_value**2)
     #plot_line_reg(timeline, slope, intercept, featurefile.replace('.json', '.png'))
-    if rsquared > 0.99:
-        filedur = get_duration(file)
-        refdur = get_duration(reffile)
-        start = intercept
-        end = filedur*slope+intercept
-        print rsquared, [0, refdur], [start, end]
-        return [start, end]
-    else:
-        print rsquared
+    filedur = get_duration(file)
+    refdur = get_duration(reffile)
+    start = intercept
+    end = filedur*slope+intercept
+    print rsquared, [0, refdur], [start, end]
+    return [start, end], rsquared
 
 def get_ref_timeline(files):
     timeline = []
@@ -89,7 +87,7 @@ def get_ref_timeline(files):
         current_time += current_duration
     return timeline
 
-def plot_timelines(timelines):
+def plot_timelines(timelines, outfile):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     max = timelines[0][-1][1]
@@ -99,7 +97,67 @@ def plot_timelines(timelines):
         for r in rects:
             ax.add_patch(r)
     fig.patch.set_facecolor('white')
-    plt.savefig(resultsdir+'timelines.png', facecolor='white', edgecolor='none')
+    plt.savefig(outfile, facecolor='white', edgecolor='none')
+
+def plot_heatmap(matrix, outfile):
+    f, ax = plt.subplots(figsize=(11, 9))
+    
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    
+    # Draw the heatmap with the mask and correct aspect ratio
+    g = sns.heatmap(matrix, cmap=cmap, #vmax=matrix.max(),
+                #square=True, xticklabels=5, yticklabels=5,#xticklabels=[-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7], yticklabels=[0,1,2,3,4,5,6,7,8,9],
+                #linewidths=.5, cbar_kws={"shrink": .5}, ax=ax,
+                mask=np.isnan(matrix))
+    sns.set_style("dark")
+    #g.set(xlabel="segment duration "+r"$2^\sigma$", ylabel="number of segments "+r"$2^\rho$")
+    plt.savefig(outfile)
+
+def fast_match(reffiles, otherfiles, search_deltas, reftimeline, keep_bias=False):
+    timeline = []
+    associations = []
+    confidence_matrix = np.empty((len(otherfiles), len(reffiles)))
+    confidence_matrix[:] = np.nan
+    if keep_bias:
+        ref_index = 0
+    for i in range(0, len(otherfiles)):
+        for d in search_deltas:
+            if keep_bias:
+                j = ref_index+d
+            else:
+                j = i+d
+            if 0 <= j and j < len(reffiles):
+                print i, j, d
+                points, rsquared = get_alignment_points(otherfiles[i], reffiles[j])
+                if rsquared > 0.99:
+                    timeline.append([points[0]+reftimeline[j][0], points[1]+reftimeline[j][0]])
+                    associations.append([i,j])
+                    confidence_matrix[i][j] = rsquared
+                    ref_index = j+1
+                    break
+    return timeline, associations, confidence_matrix
+
+def best_match(reffiles, otherfiles, search_deltas, reftimeline):
+    timeline = []
+    associations = []
+    confidence_matrix = np.empty((len(otherfiles), len(reffiles)))
+    confidence_matrix[:] = np.nan
+    for i in range(0, len(otherfiles)):
+        results = []
+        for d in search_deltas:
+            j = i+d
+            if 0 <= j and j < len(reffiles):
+                print i, j, d
+                points, rsquared = get_alignment_points(otherfiles[i], reffiles[j])
+                results.append([rsquared, points, j])
+                confidence_matrix[i][j] = rsquared
+        results.sort()
+        best_j = results[-1][2]
+        timeline.append([results[-1][1][0]+reftimeline[best_j][0], results[-1][1][1]+reftimeline[best_j][0]])
+        associations.append([i,best_j])
+    #NOW RESOLVE CONFLICTS!!!!
+    return timeline, associations, confidence_matrix
 
 def meta_align(audiodir, outdir):
     dirs = filter(os.path.isdir, [audiodir+f for f in os.listdir(audiodir)])
@@ -108,10 +166,7 @@ def meta_align(audiodir, outdir):
     
     reffiles = get_flac_filepaths(dirs[ref_index])
     reftimeline = get_ref_timeline(reffiles)
-    
     dirs.pop(ref_index)
-    
-    #get_alignment_points(get_flac_filepaths(dirs[0])[0], reffiles[0])
     
     timelines = [reftimeline]
     associations = []
@@ -119,25 +174,12 @@ def meta_align(audiodir, outdir):
     search_deltas = [0,1,-1,2,-2]
     for dir in dirs:
         currentfiles = get_flac_filepaths(dir)
-        current_timeline = []
-        current_associations = []
-        ref_index = 0
-        for i in range(0, len(currentfiles)):
-            for d in search_deltas:
-                j = ref_index+d
-                if 0 <= j and j < len(reffiles):
-                    print i, ref_index, d
-                    points = get_alignment_points(currentfiles[i], reffiles[j])
-                    if points is not None:
-                        current_timeline.append([points[0]+reftimeline[j][0], points[1]+reftimeline[j][0]])
-                        current_associations.append([i,j])
-                        ref_index = j+1
-                        break
-        #print current_timeline
-        timelines.append(current_timeline)
-        associations.append(current_associations)
+        timeline, associations, confidence_matrix = best_match(reffiles, currentfiles, search_deltas, reftimeline)
+        timelines.append(timeline)
+        associations.append(associations)
+        plot_heatmap(confidence_matrix, resultsdir+'confidence_'+dir.replace('/','_')+'_best.png')
     
-    plot_timelines(timelines)
+    plot_timelines(timelines, resultsdir+'timelines_best.png')
     print associations
     
     #pyplot some alignments
