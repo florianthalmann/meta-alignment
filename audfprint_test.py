beb5d8d37b9f519c5053ad7b55d8a8e15ae218a6
florianthalmann <florianthalmann@bluewin.ch>
2018-02-16 22:17:01 +0000

    added configs for audfprint

--- audfprint_test.py
+++ audfprint_test.py
@@ -2,6 +2,10 @@ import os, json
 from audfprint import audfprint as fp
 
 audiodir = 'audio/'
+default_config = ['--density', '70', '--fanout', '8', '--bucketsize', '500',
+    '--search-depth', '2000', '--min-count', '5']
+tiny_config = ['--density', '20', '--fanout', '3', '--bucketsize', '100',
+    '--search-depth', '100', '--min-count', '5']
 
 dirs = filter(os.path.isdir, [audiodir+f for f in os.listdir(audiodir)])
 
@@ -9,16 +13,16 @@ def create_dbs():
     #create dbs
     for d in dirs:
         fs = [d+'/'+f for f in os.listdir(d) if f.endswith('.flac')]
-        fp.main([None, 'new', '--dbase', d+'.plkz']+fs)
+        fp.main([None, 'new', '--dbase', 'def_'+d+'.plkz']+default_config+fs)
 
 def find_matches():
     matches = []
     for d in dirs:
         fs = [d+'/'+f for f in os.listdir(d) if f.endswith('.flac')]
         for e in dirs:
-            matches.append(fp.main([None, 'match', '--dbase', e+'.plkz', '--find-time-range']+fs))
-    return matches
-
-matches = find_matches()
-with open('results/fp_matches.json', 'w') as outfile:
-    json.dump(matches, outfile)
+            matches.append(fp.main([None, 'match', '--dbase', 'def_'+e+'.plkz',
+                '--find-time-range']+default_config+fs))
+    with open('results/fp_matches.json', 'w') as outfile:
+        json.dump(matches, outfile)
+create_dbs()
+#find_matches()
